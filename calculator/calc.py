31c7d42dd1005c3dbfb8fe1e8106f65ca67d5a43
Polonium <33232119+Polonium8400@users.noreply.github.com>
2017-11-22 19:46:14 +0000

    Update calc.py
    i should really use something other to elif loops

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,5 +1,33 @@
-y = input(float("enter the decided variable for x:")
-x = input(float("enter the decided variable for x:)
-sum = x + y
+ print ("type 'break' to end the program')	
+ print ("type 'add' ; 'sub' ;'mult' ;and 'div')	
+ print ("type input here:")
+	input=user_input(":")
+ if user_input == "break":
+  break
+ elif user_input == "add":
+  y = input(float("enter the decided variable for y:")
+  x = input(float("enter the decided variable for x:")
+  sum = (x + y)
 print (sum)
-
+elif user_input == "sub"
+ y = input(float("enter the decided variable for y:")
+ x = input(float("enter the decided variable for x:")
+sum = (x-y)
+	print(sum)
+elif user_input == 'mult'
+  y = input(float("enter the decided variable for y:")
+  x = input(float("enter the decided variable for x:")
+   sum = (x*y)
+  print (sum)
+ elif user_input == 'div'
+    y = input(float("enter the decided variable for y:")
+    x = input(float("enter the decided variable for x:")
+    sum = (x//y)
+    print (sum)
+  elif user_input == "expo"
+    y = input(float("enter the decided variable for y:")
+    x = input(float("enter the decided variable for x:")   
+    sum = x**y
+    print(sum)
+   else 
+             print("no detected input , plz try again")
