5afbfdb1d041297193ef565b73c7dd24a5bab5c3
Polonium <33232119+Polonium8400@users.noreply.github.com>
2017-11-25 11:17:00 +0000

    float change
    the float(input(..)) was edited as it was needed to be flipped as it caused a fail in the bot checker

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,33 +1,33 @@
  print ("type 'break' to end the program')	
- print ("type 'add' ; 'sub' ;'mult' ;and 'div')	
+ print ("type 'add' ; 'sub' ;'mult' ;and 'div'; and 'expo'")	
  print ("type input here:")
 	input=user_input(":")
  if user_input == "break":
   break
  elif user_input == "add":
-  y = input(float("enter the decided variable for y:")
-  x = input(float("enter the decided variable for x:")
+  y = float(input("enter the decided variable for y:"))
+  x = float(input("enter the decided variable for x:"))
   sum = (x + y)
-print (sum)
-elif user_input == "sub":
- y = input(float("enter the decided variable for y:")
- x = input(float("enter the decided variable for x:")
-sum = (x-y)
-	print(sum)
-elif user_input == 'mult':
-  y = input(float("enter the decided variable for y:")
-  x = input(float("enter the decided variable for x:")
-   sum = (x*y)
   print (sum)
+elif user_input == "sub":
+  y = float(input("enter the decided variable for y:"))
+  x = float(input("enter the decided variable for x:"))
+  sum = (x-y)
+  print(sum)
+elif user_input == 'mult':
+   y = float(input("enter the decided variable for y:"))
+   x = float(input("enter the decided variable for x:"))
+   sum = (x*y)
+   print (sum)
  elif user_input == 'div':
-    y = input(float("enter the decided variable for y:")
-    x = input(float("enter the decided variable for x:")
+    y = float(input("enter the decided variable for y:"))
+    x = float(input("enter the decided variable for x:"))
     sum = (x//y)
     print (sum)
   elif user_input == "expo":
-    y = input(float("enter the decided variable for y:")
-    x = input(float("enter the decided variable for x:")   
+    y = float(input("enter the decided variable for y:"))
+    x = float(input("enter the decided variable for x:"))
     sum = x**y
     print(sum)
    else: 
-             print("no detected input , plz try again")
+    print("no detected input , plz try again")
