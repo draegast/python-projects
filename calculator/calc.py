e609d67ccb1640e913a0d4792f764b58f077ce5a
Polonium <33232119+Polonium8400@users.noreply.github.com>
2017-11-27 17:10:46 +0000

    removed 'break'
    also edited to rest to operate correctly

--- calculator/calc.py
+++ calculator/calc.py
@@ -2,9 +2,8 @@ print ("type 'break' to end the program")
 print ("type 'add' ; 'sub' ;'mult' ;and 'div'; and 'expo'")	
 print ("type input here:")
 input=user_input(": ")
-if user_input == "break":
-    break
-elif user_input == "add":
+
+if user_input == "add":
     y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
     sum = (x + y)
