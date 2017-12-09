a008e66f4c3279a6db0ed45e1296284c6f4558c4
Jamie Lowry <33232119+Polonium8400@users.noreply.github.com>
2017-12-09 12:11:24 +0000

    FUNCTIONS

--- calculator/calc.py
+++ calculator/calc.py
@@ -6,8 +6,21 @@ user_input = input(": ")
 def doub(x):
     return x*2
 
-def add(x,y)
+def add(x,y):
     return x +y
+
+def sub(x,y):
+    return x-y
+
+def mult (x,y):
+    return x*y
+
+def div (x,y):
+    return x//y
+
+def expo (x,y):
+    return x**y
+
 if user_input == "add":
     x = float(input("enter the decided variable for x: "))
     y = float(input("enter the decided variable for y: "))
@@ -17,25 +30,25 @@ if user_input == "add":
  elif user_input == "sub":
     x = float(input("enter the decided variable for x: "))
     y = float(input("enter the decided variable for y: "))
-    sum = (x-y)
+    sum = sub(x,y)
     print(sum)
     
 elif user_input == 'mult':
     x = float(input("enter the decided variable for x: "))
     y = float(input("enter the decided variable for y: "))
-    sum = (x*y)
+    sum = mult(x,y)
     print(sum)
     
 elif user_input == 'div':
     x = float(input("enter the decided variable for x: "))
     y = float(input("enter the decided variable for y: "))
-    sum = (x//y)
+    sum = div(x,y)
     print(sum)
     
 elif user_input == "expo":
     x = float(input("enter the decided variable for x: "))
     y = float(input("enter the decided variable for y: "))
-    sum = x**y
+    sum = expo(x,y)
     print(sum)
     
 elif user_input == "THE ANSWER":
