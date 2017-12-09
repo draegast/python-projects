21563e2dcbf3c3e3be6f3f6aa01110e21cbb1336
draegast <34370021+draegast@users.noreply.github.com>
2017-12-09 11:26:21 +0000

    changed variables names

--- calculator/calc.py
+++ calculator/calc.py
@@ -3,28 +3,28 @@ print ("type 'add' ; 'sub' ;'mult' ;and 'div'; and 'expo'")
 print ("type input here:")
 user_input = input(": ")
 if user_input == "add":
-    y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
+    y = float(input("enter the decided variable for y: "))
     sum = (x + y)
     print(sum)
 elif user_input == "sub":
-    y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
+    y = float(input("enter the decided variable for y: "))
     sum = (x-y)
     print(sum)
 elif user_input == 'mult':
-    y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
+    y = float(input("enter the decided variable for y: "))
     sum = (x*y)
     print(sum)
 elif user_input == 'div':
-    y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
+    y = float(input("enter the decided variable for y: "))
     sum = (x//y)
     print(sum)
 elif user_input == "expo":
-    y = float(input("enter the decided variable for y: "))
     x = float(input("enter the decided variable for x: "))
+    y = float(input("enter the decided variable for y: "))
     sum = x**y
     print(sum)
 
