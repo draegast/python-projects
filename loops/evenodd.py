7db79d712a5910fb8ead7929c7c4e528eacae89f
draegast <34370021+draegast@users.noreply.github.com>
2018-02-24 03:47:34 -0800

    Update evenodd.py

--- loops/evenodd.py
+++ loops/evenodd.py
@@ -1,8 +1,6 @@
 n = int(input("input a number:  "))
-
 # If % 2 is 0, the number is even.
-if  n % 2 == 0:
- print( "{} is odd")
- print (str(n) + "is even.")
- 
-else ":"
+if n % 2 == 0:
+    print (str(n) + " is even")
+else:
+    print (str(n) + " is odd")
